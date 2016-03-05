e937181eb1c99370c0aa2b2ceb3ea24767829134
Thomas Grappin <tgrappin1986@gmail.com>
2016-03-05 11:01:00 +0100

    Ajout des composants Header, Menu et Footer

---
+++ public/app/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component }            from 'angular2/core';
+
+@Component({
+  selector: 'header',
+  template: `<header></header>`
+})
+
+export class HeaderComponent { }
---
+++ public/app/components/menu/menu.component.ts
@@ -0,0 +1,8 @@
+import { Component }            from 'angular2/core';
+
+@Component({
+  selector: 'menu',
+  template: `<menu></menu>`
+})
+
+export class MenuComponent { }
